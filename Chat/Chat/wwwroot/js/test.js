290c8c80d8680651d3df8565de28b06897ebf66d
Vladislav Grebenyuk <v.grebenyuk@scout-gps.ru>
2019-10-26 17:28:37 +0300

    Init project

---
+++ Chat/Chat/wwwroot/js/test.js
@@ -0,0 +1,13 @@
+﻿function getItems() {
+    fetch('https://localhost:44395/api/auth')
+        .then(response => response.json())
+        .then(data => _displayItems(data))
+        .catch(error => console.error('Unable to get items.', error)); 
+    alert('adada');
+}
+
+function _displayItems(data) {
+    var tBody = document.getElementById('values');
+    tBody.innerHTML = '' + data.length;
+    alert('adada2');
+}
